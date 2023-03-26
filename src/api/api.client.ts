cf3dba2587ee21a04792c247cee3f671adb6f117
Marc Gurt <marc.gurt@mapex.io>
2023-03-26 17:46:29 +0200

    added: logging for environment variables

--- src/api/api.client.ts
+++ src/api/api.client.ts
@@ -1,6 +1,10 @@
 import axios from "axios";
 
 const baseUrl = import.meta.env.VITE_API_BASE_URL as string;
+
+console.log(baseUrl);
+console.log(import.meta.env);
+
 const apiClient = axios.create({
   baseURL: baseUrl.endsWith("/api") ? baseUrl : `${baseUrl}`,
   timeout: 5000,
