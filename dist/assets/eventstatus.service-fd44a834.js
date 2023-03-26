449117f8c0ec3c273ba06bac8e1ef23a4207cde7
Marc Gurt <marc.gurt@mapex.io>
2023-03-26 18:03:57 +0200

    changed: upgrade github action

--- dist/assets/eventstatus.service-fd44a834.js
+++
@@ -1,1 +0,0 @@
-import{G as s,R as t,M as r,B as a}from"./index-7b16a937.js";class o extends a{constructor(e){super(e)}}const u=new o(s(r.Config,t.EventStatus));export{u as e};
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
   base: "./",
   plugins: [vue()],
-  server:{
-    port:8100,
-  }
+  server: {
+    port: 8100,
+  },
 });
