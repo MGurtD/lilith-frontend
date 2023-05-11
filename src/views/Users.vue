1533546f5454aa2f40d9bf82df7b9f1ec0ef793e
Zapater <eric.zapater@accenture.com>
2023-05-11 16:19:00 +0200

    Fix user.service/user view

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,23 +1,30 @@
 <template>
     <div class="card">
         <DataTable :value="users" removableSort tableStyle="min-width: 50rem">
-            <Column field="code" header="Code" sortable style="width: 25%"></Column>
-            <Column field="name" header="Name" sortable style="width: 25%"></Column>
-            <Column field="category" header="Category" sortable style="width: 25%"></Column>
-            <Column field="quantity" header="Quantity" sortable style="width: 25%"></Column>
+            <Column field="id" header="Id" sortable style="width: 20%"></Column>
+            <Column field="username" header="Username" sortable style="width: 20%"></Column>
+            <Column field="firstName" header="Firstname" sortable style="width: 20%"></Column>
+            <Column field="lastName" header="Lastname" sortable style="width: 20%"></Column>
+            <Column field="disabled" header="Disabled" sortable style="width: 20%"></Column>
         </DataTable>
     </div>
 </template>
 
 <script lang="ts">
-import { ref, onMounted } from 'vue';
-import { UserService } from '../api/services/user.service';
+import { defineComponent } from 'vue';
+import { User, UserService } from '../api/services/user.service';
 
-onMounted(() => {
-    UserService
+export default defineComponent({
+    data() {
+        return {
+            users: undefined as Array<User> | undefined,
+            userService: new UserService(),
+        };
+    },
+    async created() {        
+        this.users = await this.userService.GetAll();
+    },
 });
-
-const products = ref();
 
 </script>
   
