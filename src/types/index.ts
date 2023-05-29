fbfd04b6c3e300fa37009afdce33175f68963e93
EricZapater <ezapaterm@gmail.com>
2023-05-29 15:56:05 +0200

    Add customer service, store, type and first attempt of view

--- src/types/index.ts
+++ src/types/index.ts
@@ -5,6 +5,49 @@ export interface AuthenticationResponse {
   errors: Array<string>;
 }
 
+export interface CustomerType {
+  id: string;
+  name: string;
+  disabled: boolean;
+}
+
+export interface CustomerContact {
+  id: string;
+  firstName: string;
+  lastName: string;
+  charge: string;
+  email: string;
+  phoneNumber: string;
+  disabled: boolean;
+  mainContact: boolean;
+}
+
+export interface Customer {
+  id: string;
+  code:string;
+  comercialName: string;
+  taxName: string;
+  vatNumber: string;
+  web: string;
+  disabled: boolean;
+  customerTypeId: string;
+  contacts: Array<CustomerContact>;
+  addresses: Array<CustomerAddress>;
+}
+
+export interface CustomerAddress {
+  id: string;
+  name: string;
+  mainAddress: boolean;
+  address: string;
+  addressExtraInfo: string;
+  country: string;
+  region: string;
+  city: string;
+  postalCode: string;
+  disabled: boolean;
+}
+
 export interface SupplierType {
   id: string;
   name: string;
