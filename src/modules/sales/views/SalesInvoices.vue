945d22f84c2df0a38f76fe43a3f6973dcfc5278d
Marc Gurt <marc.gurt@mapex.io>
2023-08-17 19:19:30 +0200

    added: table TableSalesInvoice component changed: us component on salesInvoices view

--- src/modules/sales/views/SalesInvoices.vue
+++ src/modules/sales/views/SalesInvoices.vue
@@ -1,124 +1,67 @@
 <template>
-  <div>
-    <DataTable
-      class="small-datatable"
-      tableStyle="min-width: 100%"
-      scrollable
-      scrollHeight="75vh"
-      sortMode="multiple"
-      :value="invoiceStore.invoices"
-      @row-click="editSalesInvoice"
-    >
-      <template #header>
-        <div
-          class="flex flex-wrap align-items-center justify-content-between gap-2"
-        >
-          <div class="datatable-filter">
-            <div class="filter-field">
-              <label class="block text-900 mb-2">Període</label>
-              <Calendar
-                v-model="filter.dates"
-                selectionMode="range"
-                dateFormat="dd/mm/yy"
-              />
-            </div>
-            <div class="filter-field">
-              <label class="block text-900 mb-2">Client</label>
-              <Dropdown
-                v-model="filter.customerId"
-                editable
-                :options="customersStore.customers"
-                optionValue="id"
-                optionLabel="comercialName"
-                class="w-full"
-              />
-            </div>
-          </div>
-          <div class="datatable-buttons">
-            <Button
-              class="datatable-button mr-2"
-              :icon="PrimeIcons.FILTER"
-              rounded
-              raised
-              @click="filterInvoices"
+  <TableInvoices
+    :invoices="invoiceStore.invoices"
+    :customers="customersStore.customers"
+    @edit="editSalesInvoice"
+    @delete="deleteSalesInvoice"
+  >
+    <template #filter>
+      <div
+        class="flex flex-wrap align-items-center justify-content-between gap-2"
+      >
+        <div class="datatable-filter">
+          <div class="filter-field">
+            <label class="block text-900 mb-2">Període</label>
+            <Calendar
+              v-model="filter.dates"
+              selectionMode="range"
+              dateFormat="dd/mm/yy"
             />
-            <Button
-              :icon="PrimeIcons.PLUS"
-              rounded
-              raised
-              @click="createButtonClick"
+          </div>
+          <div class="filter-field">
+            <label class="block text-900 mb-2">Client</label>
+            <Dropdown
+              v-model="filter.customerId"
+              editable
+              :options="customersStore.customers"
+              optionValue="id"
+              optionLabel="comercialName"
+              class="w-full"
             />
           </div>
         </div>
-      </template>
-      <Column
-        field="invoiceNumber"
-        header="Número"
-        :sortable="true"
-        style="width: 10%"
-      ></Column>
-      <Column header="Client" style="width: 15%">
-        <template #body="slotProps">
-          {{ getSupplierNameById(slotProps.data.customerId) }}
-        </template>
-      </Column>
-      <Column header="Estat" style="width: 15%">
-        <template #body="slotProps">
-          {{ getStatusNameById(slotProps.data.statusId) }}
-        </template>
-      </Column>
-      <Column
-        header="Data"
-        field="purchaseInvoiceDate"
-        sortable
-        style="width: 15%"
-      >
-        <template #body="slotProps">
-          {{ formatDate(slotProps.data.invoiceDate) }}
-        </template>
-      </Column>
-      <Column header="Venciment" style="width: 15%">
-        <template #body="slotProps">
-          {{ getLastDueDate(slotProps.data) }}
-        </template>
-      </Column>
-      <Column header="Import" style="width: 15%">
-        <template #body="slotProps">
-          {{ slotProps.data.netAmount }} €
-        </template>
-      </Column>
-      <Column style="width: 5%">
-        <template #body="slotProps">
-          <i
-            v-if="
-              getStatusNameById(slotProps.data.statusId) ===
-              lifecycleStore.lifecycle?.initialStatusId
-            "
-            :class="PrimeIcons.TIMES"
-            class="grid_delete_column_button"
-            @click="deleteSalesInvoice($event, slotProps.data)"
+        <div class="datatable-buttons">
+          <Button
+            class="datatable-button mr-2"
+            :icon="PrimeIcons.FILTER"
+            rounded
+            raised
+            @click="filterInvoices"
           />
-        </template>
-      </Column>
-    </DataTable>
-  </div>
+          <Button
+            :icon="PrimeIcons.PLUS"
+            rounded
+            raised
+            @click="createButtonClick"
+          />
+        </div>
+      </div>
+    </template>
+  </TableInvoices>
 </template>
 <script setup lang="ts">
-import { v4 as uuidv4 } from "uuid";
+import TableInvoices from "../components/TableInvoices.vue";
 import { useConfirm } from "primevue/useconfirm";
 import { useToast } from "primevue/usetoast";
 import { useRouter } from "vue-router";
 import { useStore } from "../../../store";
 import { useSalesInvoiceStore } from "../store/invoice";
 import { useCustomersStore } from "../store/customers";
-import { DataTableRowClickEvent } from "primevue/datatable";
-import { useSharedDataStore } from "../../shared/store/masterData";
-import { useLifecyclesStore } from "../../shared/store/lifecycle";
 import { onMounted, ref } from "vue";
 import { PrimeIcons } from "primevue/api";
 import {
   formatDateForQueryParameter,
-  formatDate,
+  getNewUuid,
 } from "../../../utils/functions";
 import { SalesInvoice } from "../types";
 
@@ -126,8 +69,6 @@ const toast = useToast();
 const confirm = useConfirm();
 const router = useRouter();
 const store = useStore();
-const sharedStore = useSharedDataStore();
-const lifecycleStore = useLifecyclesStore();
 const customersStore = useCustomersStore();
 const invoiceStore = useSalesInvoiceStore();
 
@@ -138,8 +79,6 @@ const filter = ref({
 
 onMounted(async () => {
   await customersStore.fetchCustomers();
-  await sharedStore.fetchMasterData();
-  await lifecycleStore.fetchOneByName("SalesInvoice");
 
   let startDate: Date = new Date();
   let endDate: Date = new Date();
@@ -177,48 +116,16 @@ const filterInvoices = async () => {
   }
 };
 
-const getSupplierNameById = (id: string) => {
-  const supplier = customersStore.customers?.find((s) => s.id === id);
-  if (supplier) return supplier.comercialName;
-  else return "";
-};
-
-const getStatusNameById = (id: string) => {
-  const status = lifecycleStore.lifecycle?.statuses.find((s) => s.id === id);
-  if (status) return status.name;
-  else return "";
-};
-
-const getLastDueDate = (invoice: SalesInvoice): string => {
-  if (!invoice.salesInvoiceDueDates) {
-    return "";
-  } else if (invoice.salesInvoiceDueDates.length === 0) {
-    return formatDate(invoice.invoiceDate);
-  } else {
-    return formatDate(
-      invoice.salesInvoiceDueDates[invoice.salesInvoiceDueDates.length - 1]
-        .dueDate
-    );
-  }
-};
-
 const createButtonClick = () => {
-  router.push({ path: `/SalesInvoice/${uuidv4()}` });
+  router.push({ path: `/SalesInvoice/${getNewUuid()}` });
 };
 
-const editSalesInvoice = (row: DataTableRowClickEvent) => {
-  if (
-    !(row.originalEvent.target as any).className.includes(
-      "grid_delete_column_button"
-    )
-  ) {
-    router.push({ path: `/SalesInvoice/${row.data.id}` });
-  }
+const editSalesInvoice = (invoice: SalesInvoice) => {
+  router.push({ path: `/SalesInvoice/${invoice.id}` });
 };
 
-const deleteSalesInvoice = (event: any, invoice: SalesInvoice) => {
+const deleteSalesInvoice = (invoice: SalesInvoice) => {
   confirm.require({
-    target: event.currentTarget,
     message: `Està segur que vol eliminar la factura?`,
     icon: "pi pi-question-circle",
     acceptIcon: "pi pi-check",
