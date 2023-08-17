84628aa51269eaae22e029660123def401cd74cc
Marc Gurt <marc.gurt@mapex.io>
2023-08-17 19:45:29 +0200

    added: create invoice request

--- src/modules/sales/views/SalesInvoices.vue
+++ src/modules/sales/views/SalesInvoices.vue
@@ -48,6 +48,46 @@
       </div>
     </template>
   </TableInvoices>
+
+  <Dialog
+    v-model:visible="dialogOptions.visible"
+    :header="dialogOptions.title"
+    :closable="dialogOptions.closable"
+    :modal="dialogOptions.modal"
+  >
+    <form>
+      <div class="mt-2">
+        <label class="block text-900 mb-2">Client</label>
+        <Dropdown
+          class="w-full"
+          v-model="createInvoiceRequest.customerId"
+          editable
+          :options="customersStore.customers"
+          optionValue="id"
+          optionLabel="comercialName"
+        />
+      </div>
+      <div class="mt-2">
+        <label class="block text-900 mb-2">Exercici</label>
+        <Dropdown
+          class="w-full"
+          v-model="createInvoiceRequest.exerciseId"
+          editable
+          :options="sharedData.exercises"
+          optionValue="id"
+          optionLabel="name"
+        />
+      </div>
+      <div class="mt-2">
+        <label class="block text-900 mb-2">Data Factura</label>
+        <Calendar v-model="createInvoiceRequest.invoiceDate" />
+      </div>
+
+      <footer class="mt-2">
+        <Button label="Crear" @click="createInvoice" style="float: right" />
+      </footer>
+    </form>
+  </Dialog>
 </template>
 <script setup lang="ts">
 import TableInvoices from "../components/TableInvoices.vue";
@@ -57,18 +97,22 @@ import { useRouter } from "vue-router";
 import { useStore } from "../../../store";
 import { useSalesInvoiceStore } from "../store/invoice";
 import { useCustomersStore } from "../store/customers";
-import { onMounted, ref } from "vue";
+import { useSharedDataStore } from "../../shared/store/masterData";
+import { onMounted, reactive, ref } from "vue";
 import { PrimeIcons } from "primevue/api";
 import {
+  formatDate,
   formatDateForQueryParameter,
   getNewUuid,
 } from "../../../utils/functions";
-import { SalesInvoice } from "../types";
+import { CreateInvoiceRequest, SalesInvoice } from "../types";
+import { DialogOptions } from "../../../types/component";
 
 const toast = useToast();
 const confirm = useConfirm();
 const router = useRouter();
 const store = useStore();
+const sharedData = useSharedDataStore();
 const customersStore = useCustomersStore();
 const invoiceStore = useSalesInvoiceStore();
 
@@ -76,8 +120,16 @@ const filter = ref({
   dates: undefined as Array<Date> | undefined,
   customerId: undefined as string | undefined,
 });
+const dialogOptions = reactive({
+  visible: false,
+  title: "Crear factura",
+  closable: true,
+  position: "center",
+  modal: true,
+} as DialogOptions);
 
 onMounted(async () => {
+  sharedData.fetchMasterData();
   await customersStore.fetchCustomers();
 
   let startDate: Date = new Date();
@@ -116,12 +168,26 @@ const filterInvoices = async () => {
   }
 };
 
+const createInvoiceRequest = reactive({
+  id: getNewUuid(),
+  customerId: "",
+  exerciseId: "",
+  invoiceDate: formatDate(new Date()),
+} as CreateInvoiceRequest);
+
 const createButtonClick = () => {
-  router.push({ path: `/SalesInvoice/${getNewUuid()}` });
+  dialogOptions.visible = true;
+};
+
+const createInvoice = async () => {
+  const created = await invoiceStore.Create(createInvoiceRequest);
+  if (created) {
+    router.push({ path: `/sales-invoice/${createInvoiceRequest.id}}` });
+  }
 };
 
 const editSalesInvoice = (invoice: SalesInvoice) => {
-  router.push({ path: `/SalesInvoice/${invoice.id}` });
+  router.push({ path: `/sales-invoice/${invoice.id}` });
 };
 
 const deleteSalesInvoice = (invoice: SalesInvoice) => {
