2f63eed802bcb61b6d3ce824745a34d7da553bef
Marc Gurt <marc.gurt@mapex.io>
2023-10-02 13:51:29 +0200

    released

---
+++ dist/assets/masterData-609dcbb7.js
@@ -0,0 +1,1 @@
+import{I as t}from"./index-d7e0b608.js";import{S as e}from"./index-49481bdd.js";const i=t({id:"sharedMasterData",state:()=>({paymentMethods:[],taxes:[],exercises:[]}),getters:{},actions:{async fetchMasterData(){this.paymentMethods=await e.PaymentMethod.getActive(),this.exercises=await e.Exercice.getActive(),this.taxes=await e.Tax.getActive()}}});export{i as u};
---
+++ dist/assets/reference.service-b31f7e17.js
@@ -0,0 +1,1 @@
+import{B as e}from"./base.service-1e1fcad7.js";class a extends e{}export{a as R};
