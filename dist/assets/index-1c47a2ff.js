55b1116c67d20791abea99a16b4d0961459ee7e5
Marc Gurt <marc.gurt@mapex.io>
2024-01-31 16:39:03 +0100

    removed: dist

--- dist/assets/index-1c47a2ff.js
+++
@@ -1,1 +0,0 @@
-import{F as e}from"./file.service-a28e4e6a.js";const r={File:new e};export{r as S};
--- dist/assets/stockMovement-afdea4c5.js
+++
@@ -1,1 +0,0 @@
-import{J as s}from"./index-53820b8d.js";import{S as o}from"./index-ca5a7a18.js";const r=s({id:"stockMovement",state:()=>({stockMovement:void 0,stockMovements:void 0}),getters:{},actions:{async Create(e){return o.StockMovementService.Create(e)},async GetBetweenDates(e,t){this.stockMovements=await o.StockMovementService.GetBetweenDates(e,t)}}});export{r as u};
