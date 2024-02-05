3cb99681621be24edead381d0c5176971b9c74b4
EricZapater <ezapaterm@gmail.com>
2024-02-05 18:36:02 +0100

    Release copy workmasters

---
+++ dist/assets/index-72524922.js
@@ -0,0 +1,1 @@
+import{F as e}from"./file.service-9c85962b.js";const r={File:new e};export{r as S};
--- dist/assets/index-837c88ed.js
+++
@@ -1,1 +0,0 @@
-import{F as e}from"./file.service-f079e75c.js";const r={File:new e};export{r as S};
