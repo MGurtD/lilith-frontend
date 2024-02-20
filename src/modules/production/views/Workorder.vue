f119a6ee94ea8cdc6fcdc3d8ebcd5b4657d6f7d0
Marc Gurt <marc.gurt@mapex.io>
2024-02-20 19:58:18 +0100

    added: production parts tab on workorder detail view

--- src/modules/production/views/Workorder.vue
+++ src/modules/production/views/Workorder.vue
@@ -7,34 +7,57 @@
     ></FormWorkorder>
   </header>
   <main class="main">
-    <TableWorkorderPhases
-      v-if="workorder && workorder.phases"
-      :workorder="workorder"
-      :workorderPhases="workorder.phases"
-      @add="addWorkOrderPhase"
-      @edit="editWorkOrderPhase"
-      @delete="deleteWorkOrderPhase"
-    ></TableWorkorderPhases>
+    <TabView>
+      <TabPanel header="Fases">
+        <TableWorkorderPhases
+          v-if="workorder && workorder.phases"
+          :workorder="workorder"
+          :workorderPhases="workorder.phases"
+          @add="addWorkOrderPhase"
+          @edit="editWorkOrderPhase"
+          @delete="deleteWorkOrderPhase"
+        ></TableWorkorderPhases>
+      </TabPanel>
+      <TabPanel header="Hores">
+        <TableProductionParts
+          v-if="productionPartStore.productionParts"
+          :productionParts="productionPartStore.productionParts"
+        >
+          <template #header>
+            <div
+              class="flex flex-wrap align-items-center justify-content-between gap-2"
+            >
+              <span class="text-xl text-900 font-bold">Hores</span>
+              <Button
+                :icon="PrimeIcons.PLUS"
+                rounded
+                raised
+                @click="onProductionPartAddClick"
+              />
+            </div>
+          </template>
+        </TableProductionParts>
+      </TabPanel>
+    </TabView>
   </main>
 </template>
 <script setup lang="ts">
 import FormWorkorder from "../components/FormWorkorder.vue";
 import TableWorkorderPhases from "../components/TableWorkorderPhases.vue";
+import TableProductionParts from "../components/TableProductionParts.vue";
 
 import { onMounted, ref } from "vue";
 import { useRoute, useRouter } from "vue-router";
 import { useStore } from "../../../store";
 import { useReferenceStore } from "../../shared/store/reference";
 import { useWorkOrderStore } from "../store/workorder";
+import { useProductionPartStore } from "../store/productionpart";
 import { storeToRefs } from "pinia";
 import { PrimeIcons } from "primevue/api";
 import { WorkOrder, WorkOrderPhase } from "../types";
 import { usePlantModelStore } from "../store/plantmodel";
 import { useLifecyclesStore } from "../../shared/store/lifecycle";
-import {
-  convertDateTimeToJSON,
-  formatDate,
-} from "../../../utils/functions";
+import { convertDateTimeToJSON, formatDate } from "../../../utils/functions";
 import { useToast } from "primevue/usetoast";
 
 const route = useRoute();
@@ -45,6 +68,7 @@ const lifecycleStore = useLifecyclesStore();
 const referenceStore = useReferenceStore();
 const workorderStore = useWorkOrderStore();
 const plantModelStore = usePlantModelStore();
+const productionPartStore = useProductionPartStore();
 const { workorder } = storeToRefs(workorderStore);
 const id = ref("");
 
@@ -70,6 +94,7 @@ const loadViewData = async () => {
   await workorderStore.fetchOne(id.value);
   plantModelStore.fetchActiveModel();
   lifecycleStore.fetchOneByName("WorkOrder");
+  productionPartStore.fetchByWorkOrderId(id.value);
 
   if (workorderStore.workorder) {
     workorderStore.workorder.plannedDate = formatDate(
@@ -108,6 +133,8 @@ const editWorkOrderPhase = (phase: WorkOrderPhase) => {
 const deleteWorkOrderPhase = async (phase: WorkOrderPhase) => {
   await workorderStore.deletePhase(phase.id);
 };
+
+const onProductionPartAddClick = () => {};
 </script>
 <style scoped>
 .main {
