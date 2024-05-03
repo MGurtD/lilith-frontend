428c597ab4e08a6a2c8cc118fc7e80b369d18dc1
Marc Gurt <marc.gurt@mapex.io>
2024-05-03 17:03:51 +0200

    added: component to navegate to reference detail

---
+++ src/modules/shared/components/LinkReference.vue
@@ -0,0 +1,19 @@
+<template>
+  <a
+    :href="`/reference/${referenceStore.module}/${id}`"
+    class="p-link"
+    v-if="referenceStore.module && id"
+  >
+    {{ referenceStore.getShortNameById(id) }}
+  </a>
+</template>
+<script setup lang="ts">
+import { defineProps } from "vue";
+import { useReferenceStore } from "../store/reference";
+
+defineProps<{
+  id: string;
+}>();
+
+const referenceStore = useReferenceStore();
+</script>
