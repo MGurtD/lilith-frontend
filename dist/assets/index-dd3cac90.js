72bd2645446d9348841574ed2bc51a884717a6df
Marc Gurt <marc.gurt@mapex.io>
2024-06-14 09:53:15 +0200

    release

---
+++ dist/assets/index-dd3cac90.js
@@ -0,0 +1,1 @@
+import{F as e}from"./file.service-635d810e.js";const r={File:new e};export{r as S};
--- dist/assets/index-e7f0e049.js
+++
@@ -1,1 +0,0 @@
-import{F as e}from"./file.service-ddb51cac.js";const r={File:new e};export{r as S};
