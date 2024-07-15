e6a2859278b223494cfd99422d83265cf995374d
Marc Gurt <marc.gurt@mapex.io>
2024-07-15 19:09:10 +0200

    added: user filter service, type and store

---
+++ src/store/userfilter.ts
@@ -0,0 +1,51 @@
+import { defineStore } from "pinia";
+import { UserFilter } from "../types";
+import AppServices from "../api/services";
+import { useStore } from ".";
+import { getNewUuid } from "../utils/functions";
+
+export const useUserFilterStore = defineStore("userFilterStore", {
+  state: () => {
+    return {
+      filters: [] as UserFilter[],
+    };
+  },
+  getters: {
+    getFilter(state) {
+      return function (page: string, key: string) {
+        const filter = state.filters.find(
+          (f) => f.page === page && f.key === key
+        );
+        return filter ? JSON.parse(filter.filter) : null;
+      };
+    },
+  },
+  actions: {
+    async getUserFilters(userId: string) {
+      const response = await AppServices.UserFilter.GetByUserId(userId);
+      if (response) this.filters = response;
+    },
+    async addFilter(page: string, key: string, filter: any) {
+      const store = useStore();
+      const userFilter = {
+        id: getNewUuid(),
+        userId: store.user?.id as string,
+        page,
+        key,
+        filter: JSON.stringify(filter),
+      } as UserFilter;
+
+      await AppServices.UserFilter.CreateOrUpdate(userFilter);
+      this.getUserFilters(userFilter.userId);
+    },
+    async removeFilter(page: string, key: string) {
+      const userFilter = this.filters.find(
+        (f) => f.page === page && f.key === key
+      );
+      if (userFilter) {
+        await AppServices.UserFilter.Delete(userFilter);
+        this.filters = this.filters.filter((f) => f !== userFilter);
+      }
+    },
+  },
+});
