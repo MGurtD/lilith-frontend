21c6e0e6bdaebc677eba108d8cecd853f58a8c3e
Marc Gurt <marc.gurt@mapex.io>
2024-09-03 08:04:42 +0200

    build for release

--- dist/assets/stockMovement-67eea460.js
+++
@@ -1,1 +0,0 @@
-import{N as s}from"./index-709c40c8.js";import{S as o}from"./index-68f5f867.js";const r=s({id:"stockMovement",state:()=>({stockMovement:void 0,stockMovements:void 0}),getters:{},actions:{async Create(e){return o.StockMovementService.Create(e)},async GetBetweenDates(e,t){this.stockMovements=await o.StockMovementService.GetBetweenDates(e,t)}}});export{r as u};
