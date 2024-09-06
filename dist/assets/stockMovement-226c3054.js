756ca148c8c2092186674b55e0bd5aaf70a08876
EricZapater <ezapaterm@gmail.com>
2024-09-06 13:00:11 +0200

    Fix dates to pre prod

---
+++ dist/assets/stockMovement-226c3054.js
@@ -0,0 +1,1 @@
+import{N as s}from"./index-206100ee.js";import{S as o}from"./index-3f1a9249.js";const r=s({id:"stockMovement",state:()=>({stockMovement:void 0,stockMovements:void 0}),getters:{},actions:{async Create(e){return o.StockMovementService.Create(e)},async GetBetweenDates(e,t){this.stockMovements=await o.StockMovementService.GetBetweenDates(e,t)}}});export{r as u};
