63074cbbab046c745bcd6dd02b9700b8cd73a38e
EricZapater <ezapaterm@gmail.com>
2024-09-06 13:09:11 +0200

    Fix conf to pre prod

--- dist/assets/stock-df3193f0.js
+++
@@ -1,1 +0,0 @@
-import{N as t}from"./index-206100ee.js";import{S as o}from"./index-3f1a9249.js";const c=t({id:"stock",state:()=>({stocks:void 0}),getters:{},actions:{async fetchStocks(){this.stocks=await o.Stock.getAll()}}});export{c as u};
