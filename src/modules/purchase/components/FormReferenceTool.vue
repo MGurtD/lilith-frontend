175891ce9449aad0f8e34954e9e5600397b12d2b
EricZapater <ezapaterm@gmail.com>
2024-09-25 11:00:44 +0200

    Add area field to tools form

--- src/modules/purchase/components/FormReferenceTool.vue
+++ src/modules/purchase/components/FormReferenceTool.vue
@@ -11,20 +11,34 @@
         class="w-full"
       />
     </div>
+    <div class="mt-1">
+      <label class="block text-900 mb-2">Àrea de producció</label>
+      <Dropdown
+        v-model="reference.areaId"
+        editable
+        :options="plantModelStore.areas"
+        optionValue="id"
+        optionLabel="name"
+        class="w-full"
+      />
+    </div>
   </section>
 </template>
 <script setup lang="ts">
 import { Reference } from "../../shared/types";
 import { useTaxesStore } from "../../shared/store/tax";
 import { onMounted } from "vue";
+import { usePlantModelStore } from "../../production/store/plantmodel";
 
 const props = defineProps<{
   reference: Reference;
 }>();
 
 const taxesStore = useTaxesStore();
+const plantModelStore = usePlantModelStore();
 
 onMounted(async () => {
   if (!taxesStore.taxes) await taxesStore.fetchAll();
+  await plantModelStore.fetchAreas();
 });
 </script>
