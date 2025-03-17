2584463a7c36d77c16e9231a9d5f9d7ad538f5fd
Marc Gurt <marc.gurt@mapex.io>
2025-03-17 16:37:13 +0100

    removed: distribution directory

--- dist/assets/stock-bc0a83da.js
+++
@@ -1,1 +0,0 @@
-import{N as t}from"./index-aa781fed.js";import{S as o}from"./index-a991ec6e.js";const c=t({id:"stock",state:()=>({stocks:void 0}),getters:{},actions:{async fetchStocks(){this.stocks=await o.Stock.getAll()}}});export{c as u};
