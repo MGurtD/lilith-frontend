6fcaf461bd399e5e88e748d1a327b67ef735cc75
Marc Gurt <marc.gurt@mapex.io>
2025-08-07 17:34:06 +0200

    refactor: singleton services for shared services

--- src/api/services/index.ts
+++ src/api/services/index.ts
@@ -1,7 +1,15 @@
+import { AuthenticationService } from "./authentications.service";
 import { FileService } from "./file.service";
+import { ReportService } from "./report.service";
+import { RoleService } from "./role.service";
+import { UserService } from "./user.service";
 import { UserFilterService } from "./userfilter.service";
 
 export default {
+  Authentication: new AuthenticationService(),
   File: new FileService(),
+  User: new UserService(),
   UserFilter: new UserFilterService(),
+  Role: new RoleService(),
+  Reports: new ReportService(),
 };
