ab2ce6cfbc05a1120d3ff32533af0a64915bb1a2
EricZapater <ezapaterm@gmail.com>
2025-09-10 13:16:31 +0200

    Feat: add user notes to budget and budget details

--- src/modules/sales/components/FormBudget.vue
+++ src/modules/sales/components/FormBudget.vue
@@ -66,6 +66,19 @@
           />
         </div>
       </section>
+      <section class="mt-2">
+        <div>
+          <label class="block text-900 mb-2">Notes Internes</label>
+          <textarea
+            label="Notes Internes"
+            class="w-full p-inputtext p-component"
+            rows="3"
+            cols="30"
+            placeholder="Notes internes"
+            v-model="budget.userNotes"
+          ></textarea>
+        </div>
+      </section>
       <section v-if="budget.notes && budget.notes.length > 0" class="mt-2">
         <div>
           <BaseInput
