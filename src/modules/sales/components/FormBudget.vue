57ab2eac7d5644bff40ef083be3500fdefc5ad1f
EricZapater <ezapaterm@gmail.com>
2025-09-16 16:02:09 +0200

    Add Statistics

--- src/modules/sales/components/FormBudget.vue
+++ src/modules/sales/components/FormBudget.vue
@@ -45,17 +45,26 @@
         </div>
         <div>
           <label class="block text-900 mb-2">Client</label>
-          <Dropdown
-            v-model="budget.customerId"
-            editable
-            :options="customerStore.customers"
-            optionValue="id"
-            optionLabel="comercialName"
-            class="w-full"
-            :class="{
-              'p-invalid': validation.errors.customerId,
-            }"
-          />
+          <div style="display: flex; align-items: center; gap: 0.5rem">
+            <Dropdown
+              v-model="budget.customerId"
+              editable
+              :options="customerStore.customers"
+              optionValue="id"
+              optionLabel="comercialName"
+              class="w-full"
+              :class="{
+                'p-invalid': validation.errors.customerId,
+              }"
+            />
+            <router-link
+              v-if="budget.customerId"
+              :to="`/customers/${budget.customerId}`"
+              style="color: inherit"
+            >
+              <i class="pi pi-search"></i>
+            </router-link>
+          </div>
         </div>
         <div>
           <BaseInput
@@ -63,30 +72,6 @@
             label="Dies entrega"
             id="deliveryDays"
             v-model="budget.deliveryDays"
-          />
-        </div>
-      </section>
-      <section class="mt-2">
-        <div>
-          <label class="block text-900 mb-2">Notes Internes</label>
-          <textarea
-            label="Notes Internes"
-            class="w-full p-inputtext p-component"
-            rows="3"
-            cols="30"
-            placeholder="Notes internes"
-            v-model="budget.userNotes"
-          ></textarea>
-        </div>
-      </section>
-      <section v-if="budget.notes && budget.notes.length > 0" class="mt-2">
-        <div>
-          <BaseInput
-            :type="BaseInputType.TEXT"
-            label="Notes"
-            id="notes"
-            v-model="budget.notes"
-            disabled
           />
         </div>
       </section>
